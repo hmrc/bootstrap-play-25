b73bf54d9e63cc52d008c4e3ae62a6d6e5825d52
eric.ohwotu <eric.ohwotu@digital.hmrc.gov.uk>
2018-10-15 16:05:04 +0100

    updated crypto library to 5.0.0 and fixed binding issues

--- src/main/scala/uk/gov/hmrc/play/bootstrap/FrontendModule.scala
+++ src/main/scala/uk/gov/hmrc/play/bootstrap/FrontendModule.scala
@@ -18,11 +18,11 @@ package uk.gov.hmrc.play.bootstrap
 
 import play.api.inject.Binding
 import play.api.{Configuration, Environment}
-import uk.gov.hmrc.crypto.{ApplicationCrypto, ApplicationCryptoDI}
+import uk.gov.hmrc.crypto.ApplicationCrypto
 import uk.gov.hmrc.play.bootstrap.config.CryptoValidation
 import uk.gov.hmrc.play.bootstrap.filters.AuditFilter
 import uk.gov.hmrc.play.bootstrap.filters.frontend._
-import uk.gov.hmrc.play.bootstrap.filters.frontend.crypto.{CookieCryptoFilter, DefaultCookieCryptoFilter, SessionCookieCrypto, SessionCookieCryptoProvider}
+import uk.gov.hmrc.play.bootstrap.filters.frontend.crypto._
 import uk.gov.hmrc.play.bootstrap.filters.frontend.deviceid.{DefaultDeviceIdFilter, DeviceIdFilter}
 
 class FrontendModule extends BootstrapModule {
@@ -31,7 +31,7 @@ class FrontendModule extends BootstrapModule {
     super.bindings(environment, configuration) ++ Seq(
       bind[AuditFilter].to[FrontendAuditFilter],
       bind[SessionCookieCrypto].toProvider[SessionCookieCryptoProvider],
-      bind[ApplicationCrypto].to[ApplicationCryptoDI],
+      bind[ApplicationCrypto].toProvider[ApplicationCryptoProvider],
       bind[CookieCryptoFilter].to[DefaultCookieCryptoFilter],
       bind[FrontendAuditFilter].to[DefaultFrontendAuditFilter],
       bind[DeviceIdFilter].to[DefaultDeviceIdFilter],
